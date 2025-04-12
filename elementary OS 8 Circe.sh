0e3dcd698983edd17f26e7dc1c42e424c1b224fe
Edson Jorge Francisco <Edsonjorgef1@gmail.com>
2025-04-12 13:04:12 +0200

    Update elementary OS 8 Circe.sh

--- elementary OS 8 Circe.sh
+++ elementary OS 8 Circe.sh
@@ -8,6 +8,7 @@ flatpak run io.github.pantheon_tweaks.pantheon-tweaks
 
 sudo apt install synaptic -y
 sudo apt install -y gdebi
+sudo apt install build-essential git curl -y
 
 sudo ubuntu-drivers autoinstall
 
@@ -18,7 +19,7 @@ sudo apt install build-essential git curl -y
 sudo apt install nala
 sudo nala fetch
 
-sudo nala install gnome-shell-extension-manager stacer gdebi vlc htop neofetch bpytop clang cargo libc6-i386 libc6-x32 libu2f-udev samba-common-bin exfat-fuse default-jdk curl wget unrar linux-headers-$(uname -r) linux-headers-generic git gstreamer1.0-vaapi unzip ntfs-3g p7zip htop neofetch bpytop git gcc make curl bzip2 tar chromium-browser flatpak nvidia-driver-535
+sudo nala install gnome-shell-extension-manager stacer gdebi vlc htop neofetch bpytop clang cargo libc6-i386 libc6-x32 libu2f-udev samba-common-bin exfat-fuse default-jdk curl wget unrar linux-headers-$(uname -r) linux-headers-generic git gstreamer1.0-vaapi unzip ntfs-3g p7zip htop neofetch bpytop git gcc make curl bzip2 tar flatpak nvidia-driver-535
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
